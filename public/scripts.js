fb3d83ae9e3d510d62378fa35b797659b2a1ddfa
James Wesley Goedert <jwgoedert@gmail.com>
2017-04-18 14:56:33 -0500

    updated, adding deck creator with controller, model system##

--- public/scripts.js
+++ public/scripts.js
@@ -1,8 +1,28 @@
-
 const memoryPal = angular.module('memoryPal', [
   'ngRoute',
 ]);
 memoryPal.controller('mainController', ($scope) => {
   $scope.message = 'you see me?';
+  $scope.poas =[{}];
   console.log($scope.message);
 });
+
+
+'use strict';
+
+angular.
+  module('phonecatApp').
+  config(['$locationProvider' ,'$routeProvider',
+    function config($locationProvider, $routeProvider) {
+      $locationProvider.hashPrefix('!');
+
+      $routeProvider.
+        when('/phones', {
+          template: '<phone-list></phone-list>'
+        }).
+        when('/phones/:phoneId', {
+          template: '<phone-detail></phone-detail>'
+        }).
+        otherwise('/phones');
+    }
+  ]);
--- server.js
+++ server.js
@@ -1,20 +1,30 @@
-let cards = [];
 console.log('INSIDE SERVER');
 require('dotenv').config();
 
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-const dbUrl = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds161950.mlab.com:61950/mempalace`;
-const port = process.env.PORT || 3000;
+
 const app = express();
+
+const dbUrl = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds161950.mlab.com:61950/mempalace`;
+mongoose.connect(dbUrl);
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+app.use(express.static(path.join(__dirname, 'public')));
 let db;
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 MongoClient.connect(dbUrl, (err, database) => {
   if (err) return console.log(err);
   db = database;
+  console.log({{message}});
   app.listen(port, () => console.log(`listening on ${port}.`));
 });
 
@@ -36,4 +46,5 @@ app.post('/poa', (req, res) => {
   db.collection('poa').save(req.body, (err, result) =>
     err ? console.log(err) : res.redirect('/'));
 });
+module.exports = app;
 console.log('FINISHED SERVER');
