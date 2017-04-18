75a18a75c707e4d527b364adf53a72f339d1f64c
James Wesley Goedert <jwgoedert@gmail.com>
2017-04-18 09:54:46 -0500

    commented out get request and uncommented simple get to troubleshoot heroku

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const dbUrl = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds161950.
 const port = process.env.PORT || 3000;
 const app = express();
 let db;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
 MongoClient.connect(dbUrl, (err, database) => {
   if (err) return console.log(err);
@@ -16,19 +18,19 @@ MongoClient.connect(dbUrl, (err, database) => {
   app.listen(port, () => console.log(`listening on ${port}.`));
 });
 
-app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
 app.post('/poa', (req, res) => {
   db.collection('poa').save(req.body, (err, result) =>
     err ? console.log(err) : res.redirect('/'));
 });
 
-app.get('/', (req, res) => {
-  let cursor = db.collection('poa').find().toArray(function (err, results) {
-    console.log(results);
-  });
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/public/index.html');
+//   let cursor = db.collection('poa').find().toArray(function (err, results) {
+//     console.log(results);
+//   });
+// });
 
 console.log('FINISHED SERVER');
