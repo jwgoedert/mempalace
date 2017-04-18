5b826af18e6fc0cd9dd66a36912a93d00ad2d6d0
James Wesley Goedert <jwgoedert@gmail.com>
2017-04-17 23:28:05 -0500

    console logging full collection using db.collection

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 let cards = [];
 console.log('INSIDE SERVER');
 require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const MongoClient = require('mongodb').MongoClient;
+const dbUrl = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds161950.mlab.com:61950/mempalace`;
 const port = process.env.PORT || 3000;
 const app = express();
+let db;
+
+MongoClient.connect(dbUrl, (err, database) => {
+  if (err) return console.log(err);
+  db = database;
+  app.listen(port, () => console.log(`listening on ${port}.`));
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+// app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
 app.post('/poa', (req, res) => {
-  console.log('QUOTES GOT HIT', req.body);
-  cards.push(req.body);
-  console.log("ARRAY OF CARDS", cards);
+  db.collection('poa').save(req.body, (err, result) =>
+    err ? console.log(err) : res.redirect('/'));
 });
 
-app.listen(port, (err) => err ? console.log(err) : console.log(`listening on ${port}.`));
+app.get('/', (req, res) => {
+  let cursor = db.collection('poa').find().toArray(function (err, results) {
+    console.log(results);
+  });
+});
+
 console.log('FINISHED SERVER');
