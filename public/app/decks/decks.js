db7c80c8728d393368d5eb498163c530b39895ad
James Wesley Goedert <jwgoedert@gmail.com>
2017-04-19 15:09:01 -0500

    got random combiner working

--- public/app/decks/decks.js
+++ public/app/decks/decks.js
@@ -2,14 +2,42 @@ angular.module('memoryPal.decks', [])
 
   .controller('decksController', function ($scope, Decks) { //named Decks after factory tbc
     $scope.data = {};
+    $scope.splitData = { people: [], objects: [], actions: [] };
     $scope.message = 'DECKS';
+
+    $scope.randomDeck = (data) => {
+      let shuffle = (arr) => {
+        let curr = arr.length, tempVal, randomIndex;
+        while (0 !== curr) {
+          randomIndex = Math.floor(Math.random() * curr);
+          curr -= 1;
+
+          tempVal = arr[curr];
+          arr[curr] = arr[randomIndex];
+          arr[randomIndex] = tempVal;
+        }
+        return arr;
+      }
+      let zip = (a, b, c) => {
+        return a.length ?
+          [[a[0], b[0], c[0]]].concat(zip(a.slice(1), b.slice(1), c.slice(1))) : [];
+      }
+      console.log("ZIPPED/SHUFFLED", zip(shuffle(data.people), shuffle(data.actions), shuffle(data.objects)));
+
+    };
     Decks.getAll().then(function (decks) {
       $scope.data.decks = decks;
       $scope.phrases = decks.map((el) => {
         return { phrase: `${el.person} ${el.action} a ${el.object}.` };
       });
       console.log("PHRASEARR LENGTH", $scope.phrases.length);
-      // console.log("DECKS", $scope.data.decks);
-      // console.log("Phrases", $scope.phrases);
+      $scope.split = $scope.data.decks.forEach((obj) => {
+        console.log("OBJinLOOP", obj);
+        $scope.splitData.people.push(obj.person);
+        $scope.splitData.objects.push(obj.object);
+        $scope.splitData.actions.push(obj.action);
+      });
+      console.log("SPLIT DATA", $scope.splitData);
+      console.log("RANDOM!", $scope.randomDeck($scope.splitData));
     });
   });
